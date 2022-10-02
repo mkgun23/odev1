2cd5a6f41a3db4851fd4c5a68e9c9baefc8fd7dd
mkgun23 <114816126+mkgun23@users.noreply.github.com>
2022-10-02 08:53:16 +0300

    Add files via upload
    www.patika.dev

---
+++ ödev6.sql
@@ -0,0 +1,10 @@
+--select AVG(rental_rate) from film
+
+--select count(title) from film
+--where title like 'C%'
+
+--select max(length) from film
+--where rental_rate = 0.99
+
+select count(distinct(replacement_cost)) from film
+where length > 150
