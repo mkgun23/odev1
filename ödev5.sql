663479dd5a89ac99559e4919c7603d1df47591a7
mkgun23 <114816126+mkgun23@users.noreply.github.com>
2022-10-02 08:39:28 +0300

    Add files via upload

---
+++ ödev5.sql
@@ -0,0 +1,15 @@
+--select title from film
+--where title like '%n'
+--Order by length 
+--limit 5
+
+--Select title from film
+--where title like '%n'
+--Order by length DESC
+--Offset 5
+--limit 5
+
+select * from customer
+where store_id = 1
+Order by last_name DESC
+limit 4
